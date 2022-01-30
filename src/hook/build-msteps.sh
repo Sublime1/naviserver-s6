598833b3fb3124286e1183eceac46b18a8552a44
oupfiz5 <>
2022-01-30 18:15:12 +0000

    fix: bumped s6-overlay to version 3.0.0.2
    FossilOrigin-Name: bd2db88e7c02fd04572b16ea26ffce8af76078b9eb5bee31bb8d4dbc1fc43e0b

--- src/hook/build-msteps.sh
+++ src/hook/build-msteps.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # shellcheck disable=SC1091
-set -a; source ../VERSION ; set +a;
+set -a; source ../VERSIONS ; set +a;
 source ../builds/env-vars.sh
 
-IMAGE="${IMAGE:-oupfiz5/naviserver-s6:${VERSION}}"
+IMAGE="${IMAGE:-oupfiz5/naviserver-s6:${NS_IMAGE_TAG}}"
 
 docker build --no-cache \
        --build-arg BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%SZ")" \
-       --build-arg VERSION="${VERSION}" \
+       --build-arg VERSION="${NS_IMAGE_TAG}" \
        --build-arg NS_VERSION="${NS_VERSION}" \
        --build-arg NS_MODULES_VERSION="${NS_MODULES_VERSION}" \
        --build-arg RL_JSON_VERSION="${RL_JSON_VERSION}" \
--- src/hook/build.sh
+++ src/hook/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # shellcheck disable=SC1091
-set -a; source ../VERSION ; set +a;
+set -a; source ../VERSIONS ; set +a;
 
-IMAGE="${IMAGE:-oupfiz5/naviserver-s6:${VERSION}}"
+IMAGE="${IMAGE:-oupfiz5/naviserver-s6:${NS_IMAGE_TAG}}"
 
 docker build \
        --build-arg BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%SZ")" \
-       --build-arg VERSION="${VERSION}" \
+       --build-arg VERSION="${NS_IMAGE_TAG}" \
        -t "${IMAGE}" \
        -f ../Dockerfile \
        ../.
