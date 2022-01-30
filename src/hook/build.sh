9e06b705e9c6e6731527c402aef10cdfde10003f
oupfiz5 <>
2022-01-30 04:59:26 +0000

    fix: updated finished script for naviserver s6
    FossilOrigin-Name: 2791dc75643ece6b6be225d9792bca239f51105ccd4e283e0ec493d54bce13c8

--- src/hook/build.sh
+++ src/hook/build.sh
@@ -2,7 +2,7 @@
 # shellcheck disable=SC1091
 set -a; source ../VERSION ; set +a;
 
-IMAGE="${IMAGE:-oupfiz5/naviserver-s6:${VERSION}"
+IMAGE="${IMAGE:-oupfiz5/naviserver-s6:${VERSION}}"
 
 docker build \
        --build-arg BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%SZ")" \
